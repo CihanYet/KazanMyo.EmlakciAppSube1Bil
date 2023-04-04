4fe243ff17924d33503f4a1319ba95a9520ebd8e
cihyet <cyetisken@gazi.edu.tr>
2023-04-04 14:39:20 +0300

    Kalıtım Konusu Anlatıldı

--- KazanMyo.EmlakciAppSube1/Program.cs
+++ KazanMyo.EmlakciAppSube1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KazanMyo.EmlakciLibSube1Bil;
+using System;
 
 namespace KazanMyo.EmlakciAppSube1
 {
@@ -7,58 +8,28 @@ namespace KazanMyo.EmlakciAppSube1
         static void Main(string[] args)
         {
 
-            Ev evim3 = new Ev();
-            Ev evim = new Ev(3, 2, "Gazi", 100);
-            Ev evim2 = new Ev(2, 5, 100);
+            SatilikEv se=new SatilikEv();
+            se.Katno = 2;
+            se.Alan = 100;
+            se.Semt = "Gazi";
+            se.Odasayisi = 4;
+            se.Satisfiyat = 1000000;
 
-            Console.WriteLine(evim2.EvBilgileri());
-        }
-    }
-
-    class Ev
-    {
-        public Ev()
-        {
-
-        }
-        public Ev(int odasayisi, int katno, string semt, double alan)
-        {
-            this.Odasayisi = odasayisi;
-            this.Katno = katno;
-            this.Semt = semt;
-            this.Alan = alan;
+                      
         }
 
-        public Ev(int odasayisi, int katno, double alan)
-        {
-            this.Odasayisi = odasayisi;
-            this.Katno = katno;
-            this.Semt = "Gazi";
-            this.Alan = alan;
-        }
-
-        private int odasayisi;
-
-        public int Odasayisi
-        {
-            get { return odasayisi; }
-            set { odasayisi = value; }
-        }
-
-        public string Semt { get => semt; set => semt = value; }
-        public int Katno { get => katno; set => katno = value; }
-
-        private string semt;
-        private int katno;
-        public double Alan { get; set; }
-
-        public string EvBilgileri()
-        {
-            return $"Odasayısı:{this.odasayisi}\nAlan:{this.Alan}\nSemt:{this.Semt}\nKat No:{this.Katno}";
-        }
-
-    }
+        
+    } 
 }
+
+
+
+
+
+
+
+
+
 
 //Class: Aynı zamanda bir veri tipidir.(Referans tipi) Eğer önceden tanımlanmış veri tipleri bizim ihtiyacımızı karşılamıyorsa, o zaman class yazarak ihtiyacımıza özel veri tipi oluşturabiliriz.
 //Private: Sadece tanımlandıkları class içerisinden erişilebilir üyeler
---
+++ KazanMyo.EmlakciAppSube1Bil/SatilikEv.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KazanMyo.EmlakciLibSube1Bil
+{
+    public class SatilikEv:Ev
+    {
+        public double Satisfiyat { get; set; }
+    }
+}
+
+//internal erişim belirleyicisi: Sadece tanımlı olduğu namespace içerisinde kullanılabilir.
